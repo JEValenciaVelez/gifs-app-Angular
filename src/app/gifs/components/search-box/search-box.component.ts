11e656bc3117251b4352c1e32815c7f764ec2536
unknown <juancho50301@gmail.com>
2023-08-26 16:11:05 -0500

    service gifs

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,5 +1,10 @@
 
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+
+import { GifsService } from '../../services/gifs.service';
+
+
 
 @Component({
   selector: 'gifs-search-box',
@@ -8,12 +13,32 @@ import { Component } from '@angular/core';
   <input
   type="text"
   class="form-control"
-  placeholder="Buscar gifs...">
+  placeholder="Buscar gifs..."
+  (keyup.enter)="HandleSubmit()"
+  #searchInput
+  >
   `
 })
 
 export class SearchBoxComponent  {
 
-  
+   //injectamos el servicio en el constructor
+   constructor (private gifsService : GifsService) {}
+
+  @ViewChild('searchInput')
+  public tagInput! : ElementRef<HTMLInputElement>;
+
+  HandleSubmit ():void {
+
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
+  }
+
+
+
+
+
 }
 
